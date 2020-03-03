ccab438ca10f5bc0db29f912ec5c6dc3a302c6ee
김호한 <ghgksdk123@naver,com>
2020-03-03 17:39:04 +0900

    DFS(비트마스킹을 이용한 흔적 체크) 응용문제 **

--- Swexpertacademy/Swexpertacademy_2814_최장경로.java
+++ Swexpertacademy/Swexpertacademy_2814_최장경로.java
@@ -22,7 +22,7 @@ public class 최장경로2 {
             }
             ans = 0;
             for(int i = 1; i <= N; i++) {
-                ans = Math.max(ans, dfs(i, 1<            }
+                ans = Math.max(ans, dfs(i, 1<<i }
             System.out.println("#" + tc + " " + ans);
         }
     }
